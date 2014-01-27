c4eed7e8d8bda11809c49e245b68bcd930fd242f
javiersm <javier.santana.mejias@gmail.com>
2014-01-27 23:42:39 +0100

    21-1-14

--- Deitel/src/tema17/CreateTextFile.java
+++ Deitel/src/tema17/CreateTextFile.java
@@ -20,7 +20,7 @@ public class CreateTextFile
    {
       try
       {
-         output = new Formatter( "prueba.txt" );
+         output = new Formatter( "textfields/clients.txt" );
          System.out.println(output.toString());
       } // end try
       catch ( SecurityException securityException )
@@ -103,16 +103,5 @@ public class CreateTextFile
 } // end class CreateTextFile
 
 /*************************************************************************
-* (C) Copyright 1992-2005 by Deitel & Associates, Inc. and               *
-* Pearson Education, Inc. All Rights Reserved.                           *
-*                                                                        *
-* DISCLAIMER: The authors and publisher of this book have used their     *
-* best efforts in preparing the book. These efforts include the          *
-* development, research, and testing of the theories and programs        *
-* to determine their effectiveness. The authors and publisher make       *
-* no warranty of any kind, expressed or implied, with regard to these    *
-* programs or to the documentation contained in these books. The authors *
-* and publisher shall not be liable in any event for incidental or       *
-* consequential damages in connection with, or arising out of, the       *
-* furnishing, performance, or use of these programs.                     *
+* FUNCIONA CORRECTAMENTE: se crea el directorio en el directorio principal del proyecto, dnd esta "src" y "bin"
 *************************************************************************/
