e19cd7a08ecf836324a81a89d043f2a789e9dc8a
javiersm <javier.santana.mejias@gmail.com>
2014-02-27 18:02:41 +0100

    ok

---
+++ Deitel/src/tema17/CreateRandomFileTest.java
@@ -0,0 +1,13 @@
+package tema17;
+
+// Fig. 14.25: CreateRandomFileTest.java
+// Testing class CreateRandomFile.
+
+public class CreateRandomFileTest
+{    
+   public static void main( String args[] )
+   {
+      CreateRandomFile application = new CreateRandomFile();
+      application.createFile();
+   } // end main
+} // end class CreateRandomFileTest
